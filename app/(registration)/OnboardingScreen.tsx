b3b84523c7f2faf317c1584934b2be2918e7b050
Noah-V <noahvikoo3@gmail.com>
2024-09-10 11:22:23 +0100

    Noah's Commit: Modifying Index Registration Page and Onboarding Screens

--- app/(registration)/OnboardingScreen.tsx
+++ app/(registration)/OnboardingScreen.tsx
@@ -1,18 +1,52 @@
-import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    TextInput,
-    Pressable,
-    Button,
-} from "react-native";
-import React from 'react'
-import ButtonGroup from "../../src/components/ButtonGroup";
+import { StyleSheet, View, Image, TextInput, Pressable, Button } from "react-native";
+import React, { useState } from "react";
+import { Text } from "@components/Text";
+import { Link } from "expo-router";
+import ButtonGroup from "@components/ButtonGroup";
 
-export default function OnboardingThree() {
+export default function OnboardingScreen() {
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const [finalObPage, setFinalObPage] = useState(false);
+
+    const handleNextOnboardingPage = () => {
+        if (currentPage < onboardingData.length - 1) {
+            setCurrentPage(currentPage + 1);
+        } else {
+            setFinalObPage((prevState) => !prevState);
+            console.log("Complete");
+        }
+    };
+    const onboardingData = [
+        {
+            title: "Welcome to Artisan Trades Network",
+            subtitle: "Empowering artisans to effortlessly allow them to gain more access and visibility to customers",
+            buttonText: "Next",
+        },
+        {
+            title: "Discover Powerful Features",
+            subtitle: "Bidding feature: Bid for jobs at will. Real-time chat with clients to get jobs done. E-Wallet: Get paid and accept payments.",
+            buttonText: "Next",
+        },
+        {
+            title: "How It Works",
+            subtitle: "Make bids easily on jobs posted by customers. Accept jobs, get customer feedback, and more!",
+            buttonText: "Next",
+        },
+        {
+            title: "Customization and Personalization",
+            subtitle: "Profile setup, customize your profile and preferences. Set notification settings and more.",
+            buttonText: "Next",
+        },
+        {
+            title: "Ready to Boost Your Productivity?",
+            subtitle: "Explore the dashboard and get started!",
+            buttonText: "Get Started",
+        },
+    ];
+
     return (
-        <View style={styles.container} >
+        <View style={styles.container}>
             <View style={styles.logoContainer}>
                 {/* <View style={styles.logo}> */}
                 <Image source={require("../../assets/images/logo.png")} />
@@ -24,12 +58,9 @@ export default function OnboardingThree() {
             </View>
             <View style={styles.textContainer}>
                 <View style={styles.welcomeContainer}>
-                    <Text style={styles.welcomeTextOne}>Discover Powerful</Text>
-                    <Text style={styles.welcometextTwo}>Feature</Text>
+                    <Text style={styles.welcomeText}>{onboardingData[currentPage].title}</Text>
+                    <Text style={styles.welcomeTextDesc}>{onboardingData[currentPage].subtitle}</Text>
                 </View>
-                <Text style={styles.text}>. Bidding feature: Bid for jobs at will.</Text>
-                <Text>. Real-Time chat: with clients on task to be done.</Text>
-                <Text>. Wallet: Get paid and track payments.</Text>
                 <View style={styles.navigationContainer}>
                     <View style={styles.activeNav}></View>
                     <View style={styles.barOne}></View>
@@ -38,20 +69,19 @@ export default function OnboardingThree() {
                     <View style={styles.barFour}></View>
                 </View>
             </View>
-            {/* <View style={styles.Navigation}>
-                <Text style={styles.skip}>Skip</Text>
-                <Text style={styles.next}>Next</Text>
-            </View> */}
-            {/* <ButtonGroup href={"/(registration)/OnboardingThree"} isNop negativeOption="Skip" positiveOption="Next" /> */}
-            <ButtonGroup positiveOption="Next" href={"/OnboardingFour"} negativeOption="Skip" negativeHref={"/"} reverse />
-        </View >
-    )
+            {finalObPage ? (
+                <ButtonGroup href={"/Home"} positiveOption="Proceed" paddingHorizontal={20} />
+            ) : (
+                <ButtonGroup onPress={handleNextOnboardingPage} positiveOption="Next" negativeOption="Skip" paddingHorizontal={20} reverse />
+            )}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         display: "flex",
-        height: '55%',
+        height: "55%",
         backgroundColor: "#8f8f8f",
     },
     logoContainer: {
@@ -59,26 +89,24 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        paddingTop: '7%',
+        paddingTop: "7%",
     },
     textContainer: {
         alignItems: "center",
-        display: 'flex',
-        paddingTop: '80%',
-
+        display: "flex",
+        paddingTop: "80%",
     },
     welcomeContainer: {
-
+        paddingHorizontal: 20,
     },
-    welcomeTextOne: {
-        fontSize: 33,
+    welcomeText: {
+        fontWeight: "600",
+        fontSize: 30,
+        textAlign: "center",
     },
-    welcometextTwo: {
-        fontSize: 33,
-        marginLeft: "20%",
-    },
-    text: {
-        marginTop: "3%",
+    welcomeTextDesc: {
+        textAlign: "center",
+        fontSize: 12,
     },
     navigationContainer: {
         flexDirection: "row",
@@ -91,9 +119,8 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         paddingTop: "1%",
         paddingBottom: "1%",
-        paddingLeft: "1%",
-        paddingRight: "1%",
-        marginLeft: "0.3%",
+        paddingLeft: "5%",
+        paddingRight: "5%",
         marginTop: "10%",
         alignItems: "center",
         borderColor: "#52A2f2",
@@ -105,8 +132,9 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         paddingTop: "1%",
         paddingBottom: "1%",
-        paddingLeft: "5%",
-        paddingRight: "5%",
+        paddingLeft: "1%",
+        paddingRight: "1%",
+        marginLeft: "0.3%",
         marginTop: "10%",
         alignItems: "center",
         borderColor: "#52A2f2",
@@ -162,8 +190,8 @@ const styles = StyleSheet.create({
     },
     skip: {
         borderWidth: 0.5,
-        paddingTop: "2.5%",
-        paddingBottom: "2.5%",
+        paddingTop: "3%",
+        paddingBottom: "15%",
         paddingLeft: "10%",
         paddingRight: "10%",
         marginLeft: "0.3%",
@@ -174,16 +202,15 @@ const styles = StyleSheet.create({
     },
     next: {
         borderWidth: 0.5,
-        paddingTop: "2.5%",
-        paddingBottom: "2.5%",
+        paddingTop: "3%",
+        paddingBottom: "15%",
         paddingLeft: "10%",
         paddingRight: "10%",
         marginLeft: "0.3%",
-        alignItems: "center",
         borderColor: "#52A2f2",
         backgroundColor: "#52A2f2",
         position: "relative",
+        color: "white",
         borderRadius: 5,
-    }
-
-})
+    },
+});
--- app/(registration)/_layout.tsx
+++ app/(registration)/_layout.tsx
@@ -3,34 +3,17 @@ import React from "react";
 import { Stack, useNavigation, useRootNavigation } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
-
-
 export default function Layout() {
-	const rootNavigation = useNavigation();
-	return (
-		<Stack>
-			<Stack.Screen
-				name="SignUp"
-				options={{
-					headerBackVisible: true,
-					headerLeft: () => (
-						<Pressable
-							onPress={() => rootNavigation.goBack()}
-							style={{ flexDirection: "row", alignItems: "center" }}
-						>
-							<Ionicons
-								name="chevron-back"
-								size={32}
-								color={"#007AFF"}
-								style={{ fontSize: 28 }}
-							/>
-							<Text style={{ color: "#007AFF", fontSize: 16 }}>Back</Text>
-						</Pressable>
-					),
-					// headerShown: false
-				}}
-			/>
-		</Stack>
-	);
+    const rootNavigation = useNavigation();
+    return (
+        <Stack>
+            <Stack.Screen
+                name="SignUp"
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Stack>
+    );
 }
 // export default Stack;
