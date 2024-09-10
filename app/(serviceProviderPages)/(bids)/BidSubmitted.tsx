4e344847ec5ca64d7ea0666e98b4701cb87c61d5
King Shadow <anijimjamesmaurice@gmail.com>
2024-09-10 11:40:21 +0100

    Rex's Commit: Created and completed the BidSubmitted page

---
+++ app/(serviceProviderPages)/(bids)/BidSubmitted.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import BidSubmittedSuccessfully from "../../../assets/images/BidSubmittedSuccessfuilly.svg";
+import ButtonGroup from "../../../src/components/ButtonGroup";
+import colors from "../../../src/helpers/colors";
+import { Text } from "../../../src/components/Text";
+
+const BidSubmitted = () => {
+	return (
+		<View style={styles.container}>
+			<BidSubmittedSuccessfully />
+			<View style={styles.successContainer}>
+				<Text style={styles.successTitle}>Bid Submitted Successfully</Text>
+				<View style={styles.successSubTitleContainer}>
+					<Text style={styles.successSubTitle}>
+						Lorem ipsum dolor sit amet, consecteturelit. Etiam sed metus at est
+						iaculis
+					</Text>
+				</View>
+			</View>
+			<ButtonGroup
+				positiveOptionStyle={{}}
+				positiveOption="Go back to home"
+				href={"/(home)/Home"}
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		// backgroundColor: "#ff0",
+		alignItems: "center",
+		justifyContent: "center",
+		// gap: 5,
+		// paddingTop: 70,
+	},
+
+	successContainer: {
+		justifyContent: "center",
+		alignItems: "center",
+		// paddingHorizontal: 30,
+		gap: 10,
+		// backgroundColor: "#f0f",
+	},
+
+	successTitle: {
+		fontSize: 30,
+		fontWeight: "700",
+		textAlign: "center",
+	},
+
+	successSubTitleContainer: {
+		paddingHorizontal: 40,
+	},
+
+	successSubTitle: {
+		textAlign: "center",
+		color: colors.greySecondaryShade,
+	},
+});
+
+export default BidSubmitted;
