78743419aa9b5cf812571e1e9abbb9ba815e6e26
King Shadow <anijimjamesmaurice@gmail.com>
2024-09-12 23:05:07 +0100

    Rex's Commit: Added the Recommended Jobs section in the Home page for Service Providers

--- app/(home)/Home.tsx
+++ app/(home)/Home.tsx
@@ -10,6 +10,7 @@ import HomeCarousel from "../../src/components/HomeCarousel";
 import React, { ReactElement, useEffect, useState } from "react";
 import { View, StyleSheet, ImageBackground, ScrollView, Dimensions, FlatListProps, ImagePropsBase } from "react-native";
 import { compactStyles } from "@helpers/styles";
+import { Bid, PostedBid } from "./Bids";
 
 const HomeCard1 = require("../../assets/images/homeCard1.png");
 const HomeCard2 = require("../../assets/images/homeCard2.png");
@@ -26,6 +27,24 @@ export interface SwipeData extends LinkProps {
 	icon?: ReactElement<any, any>;
 	secondIcon?: ReactElement<any, any>;
 }
+
+const recommendedJobs: Bid[] = [
+	{
+		jobTitle: "Need to repair my toilet",
+		jobServiceCategory: "Maintenance",
+		jobDetail: "Lorem dolore quis pariatur porro ullam facilis molestiae quasi.",
+		jobPrice: "50,000 - 70,000",
+		jobDate: "Posted 2 hours ago",
+	},
+	{
+		jobTitle: "Need to repair my toilet again",
+		jobServiceCategory: "Maintenance",
+		jobDetail:
+			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, aliquam Officia deserunt dicta alias dolore quis pariatur porro ullam facilis molestiae quasi.",
+		jobPrice: "50,000 - 70,000",
+		jobDate: "Posted 2 hours ago",
+	},
+];
 
 export default function Home() {
 	const styles = compactStyles(generalStyles, androidStyles, iosStyles);
@@ -67,57 +86,71 @@ export default function Home() {
 	];
 
 	return (
-		<View style={styles.container}>
+		<>
 			<View style={styles.menuHeaderContainer}>
 				<MenuHeader />
 			</View>
-			<View style={styles.contentSection}>
-				<View style={styles.salutationContainer}>
-					<Text style={styles.salutationText}>Hello</Text>
-					<Text style={styles.userName}>Nonso</Text>
-				</View>
+			<ScrollView contentContainerStyle={styles.container}>
+				<View style={styles.contentSection}>
+					<View style={styles.salutationContainer}>
+						<Text style={styles.salutationText}>Hello</Text>
+						<Text style={styles.userName}>Nonso</Text>
+					</View>
 
-				<View style={styles.cardSection}>
-					<HomeCarousel data={swipeData} />
-				</View>
+					<View style={styles.cardSection}>
+						<HomeCarousel data={swipeData} />
+					</View>
 
-				<View style={styles.rewardContainer}>
-					<View
-						style={{
-							alignItems: "center",
-							flexDirection: "row",
-							justifyContent: "center",
-							gap: 10,
-						}}
-					>
-						<RewardIcon />
-						<View style={{ width: width * 0.4 }}>
-							<Text
-								style={{
-									fontSize: 20,
-									fontWeight: "600",
-									...styles.basicTextStyle,
-								}}
-							>
-								Reward!
-							</Text>
-							<Text style={{ fontSize: 12, ...styles.basicTextStyle }}>
-								Invite your friends today and earn a reward
-							</Text>
+					<View style={styles.recommendedContainer}>
+						<View style={styles.recommendedHeader}>
+							<Text style={styles.recommendedTitle}>Recommended</Text>
+							<Text style={styles.recommendedViewAll}>View all</Text>
+						</View>
+						<View style={styles.recommendedJobContainer}>
+							{recommendedJobs.map((recommendedJob, index) => (
+								<PostedBid job={recommendedJob} key={index} containerStyle={styles.recommendedJob} />
+							))}
 						</View>
 					</View>
-					<View
-						style={{
-							backgroundColor: colors.greySecondaryShade,
-							borderRadius: 20,
-							padding: 5,
-						}}
-					>
-						<Ionicons name="chevron-forward" size={25} color={"white"} />
+
+					<View style={styles.rewardContainer}>
+						<View
+							style={{
+								alignItems: "center",
+								flexDirection: "row",
+								justifyContent: "center",
+								gap: 10,
+							}}
+						>
+							<RewardIcon />
+							<View style={{ width: width * 0.4 }}>
+								<Text
+									style={{
+										fontSize: 20,
+										fontWeight: "600",
+										...styles.basicTextStyle,
+									}}
+								>
+									Reward!
+								</Text>
+								<Text style={{ fontSize: 12, ...styles.basicTextStyle }}>
+									Invite your friends today and earn a reward
+								</Text>
+							</View>
+						</View>
+						<View
+							style={{
+								backgroundColor: colors.greySecondaryShade,
+								borderRadius: 20,
+								padding: 5,
+							}}
+						>
+							<Ionicons name="chevron-forward" size={25} color={"white"} />
+						</View>
 					</View>
 				</View>
-			</View>
-		</View>
+			</ScrollView>
+		</>
 	);
 }
 
@@ -128,23 +161,25 @@ const androidStyles = StyleSheet.create({
 		backgroundColor: "#fff",
 		alignItems: "center",
 		paddingHorizontal: 20,
+		paddingBottom: 20,
 	},
+
 	container: {
-		flex: 1,
+		// flex: 1,
 		backgroundColor: "#fff",
 		alignItems: "center",
-		gap: 50,
+		paddingBottom: "35%",
 	},
 
 	contentSection: {
 		width: "100%",
 		gap: 5,
-		paddingLeft: "10%",
 	},
 
 	salutationContainer: {
 		flexDirection: "row",
 		alignItems: "flex-start",
+		paddingHorizontal: "10%",
 	},
 	salutationText: {
 		color: colors.greyShade,
@@ -159,14 +194,42 @@ const androidStyles = StyleSheet.create({
 		alignItems: "flex-end",
 	},
 
+	recommendedContainer: {
+		marginTop: "5%",
+		marginBottom: "5%",
+	},
+
+	recommendedHeader: {
+		paddingHorizontal: "10%",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+
+	recommendedTitle: {
+		fontSize: 20,
+		fontWeight: "600",
+	},
+
+	recommendedViewAll: {
+		color: "#4C4C4C",
+		textDecorationLine: "underline",
+	},
+
+	recommendedJobContainer: {},
+
+	recommendedJob: {
+		paddingHorizontal: "10%",
+	},
+
 	rewardContainer: {
 		backgroundColor: colors.brownShade,
 		height: height * 0.125,
-		width: width * 0.8,
 		borderRadius: 15,
 		marginTop: 10,
 		flexDirection: "row",
 		padding: 20,
+		marginHorizontal: "10%",
 		justifyContent: "space-between",
 		alignItems: "center",
 	},
@@ -187,23 +250,24 @@ const iosStyles = StyleSheet.create({
 		backgroundColor: "#fff",
 		alignItems: "center",
 		paddingHorizontal: 20,
+		paddingBottom: 20,
 	},
 	container: {
 		flex: 1,
 		backgroundColor: "#fff",
 		alignItems: "center",
-		gap: 50,
+		paddingBottom: "35%",
 	},
 
 	contentSection: {
 		width: "100%",
 		gap: 5,
-		paddingLeft: "10%",
 	},
 
 	salutationContainer: {
 		flexDirection: "row",
 		alignItems: "flex-start",
+		paddingHorizontal: "10%",
 	},
 	salutationText: {
 		color: colors.greyShade,
@@ -218,14 +282,42 @@ const iosStyles = StyleSheet.create({
 		alignItems: "flex-end",
 	},
 
+	recommendedContainer: {
+		marginTop: "5%",
+		marginBottom: "5%",
+	},
+
+	recommendedHeader: {
+		paddingHorizontal: "10%",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+
+	recommendedTitle: {
+		fontSize: 20,
+		fontWeight: "600",
+	},
+
+	recommendedViewAll: {
+		color: "#4C4C4C",
+		textDecorationLine: "underline",
+	},
+
+	recommendedJobContainer: {},
+
+	recommendedJob: {
+		paddingHorizontal: "10%",
+	},
+
 	rewardContainer: {
 		backgroundColor: colors.brownShade,
 		height: height * 0.125,
-		width: width * 0.8,
 		borderRadius: 15,
 		marginTop: 10,
 		flexDirection: "row",
 		padding: 20,
+		marginHorizontal: "10%",
 		justifyContent: "space-between",
 		alignItems: "center",
 	},
