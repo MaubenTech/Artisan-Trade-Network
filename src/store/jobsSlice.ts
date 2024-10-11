76fe918b3de133d8ddb5e901742fde3add0f4e88
King Shadow <anijimjamesmaurice@gmail.com>
2024-10-11 14:27:45 +0100

    Rex's Commit: Commenting out the api calls

--- src/store/jobsSlice.ts
+++ src/store/jobsSlice.ts
@@ -22,67 +22,70 @@ export interface Job {
 	updatedAt: string; //TODO: Check if this property can use the Date type instead of string, to keep formatting, etc.
 }
 
-export const fetchJobs = createAsyncThunk<Job[], void>('jobs/FetchJobs', async () => {
-	const jobs = await getData<Job[]>('/jobs');
-	return jobs;
-})
+// export const fetchJobs = createAsyncThunk<Job[], void>("jobs/FetchJobs", async () => {
+// 	const jobs = await getData<Job[]>("/jobs");
+// 	return jobs;
+// });
 
-const jobs = getData('/jobs');
-console.log('Jobs:', jobs);
+// const jobs = getData("/jobs");
+// console.log("Jobs:", jobs);
 
-interface JobState{
+interface JobState {
 	jobs: Job[];
 	loading: boolean;
 	error: string | null;
 }
 
-const dummyJob: Job[] = [{
-	_id: "1",
-	title: "IDK",
-	type: "Mechanic",
-	description: "Well well well",
-	budget: "50000",
-	address: "No 1 Two Street, Three City, Four State",
-	service: "A very good service",
-	media: [
-		{
-			url: "nothing_yet",
-			type: "image",
-		},
-	],
-	userId: "1",
-	status: "Posted",
-	createdAt: new Date().toString(),
-	updatedAt: new Date().toString(),
-}];
+const dummyJob: Job[] = [
+	{
+		_id: "1",
+		title: "IDK",
+		type: "Mechanic",
+		description: "Well well well",
+		budget: "50000",
+		address: "No 1 Two Street, Three City, Four State",
+		service: "A very good service",
+		media: [
+			{
+				url: "nothing_yet",
+				type: "image",
+			},
+		],
+		userId: "1",
+		status: "Posted",
+		createdAt: new Date().toString(),
+		updatedAt: new Date().toString(),
+	},
+];
 
 const initialState: JobState = {
-	jobs: [
-		...dummyJob
-	],
+	jobs: [...dummyJob],
 	loading: false,
 	error: null,
-}
+};
 
 const jobSlice = createSlice({
 	name: "jobs",
 	initialState,
 	reducers: {},
-	extraReducers(builder) {
-		builder
-			.addCase(fetchJobs.pending, state => {
-				state.loading = true;
-				state.error = null;
-			})
-			.addCase(fetchJobs.fulfilled, (state, action) => {
-				state.jobs = action.payload
-			})
-			.addCase(fetchJobs.rejected, (state, action) => {
-				state.loading = false;
-				state.error = action.error.message || 'Failed to fetch Jobs'
-				console.error('Failed to fetch Jobs');
-			})
-	},
+	// extraReducers: (builder) => {
+	// 	builder
+	// 		.addCase(fetchJobs.pending, (state) => {
+	// 			state.loading = true;
+	// 			state.error = null;
+	// 		})
+	// 		.addCase(fetchJobs.fulfilled, (state, action) => {
+	// 			state.loading = false;
+	// 			state.error = null;
+	// 			// state.jobs = action.payload;
+	// 		})
+	// 		.addCase(fetchJobs.rejected, (state, action) => {
+	// 			state.loading = false;
+	// 			state.error = action.error.message || "Failed to fetch Jobs";
+	// 			console.error("Failed to fetch Jobs");
+	// 		});
+	// },
+	// TODO: When we start connecting to the api, this becomes useful
 });
 
 export const {} = jobSlice.actions;
