b986603dddc37b4ecf3e3a5284a3a44a58ab76a1
King Shadow <anijimjamesmaurice@gmail.com>
2024-10-14 04:50:56 +0100

    Rex's Commit: Progress on the bidSlice

--- src/store/bidsSlice.ts
+++ src/store/bidsSlice.ts
@@ -1,34 +1,36 @@
+import createAppSelector from "@hooks/createAppSelector";
 import { createSlice } from "@reduxjs/toolkit";
 import { BidStatus } from "app/(home)/Bids";
+import { selectJobById } from "./jobsSlice";
 
-interface Bid {
+export interface BidState {
 	_id: string;
 	artisanId: string;
 	jobId: string;
-	bidPrice: number;
-	description: string;
+	bidPrice: string;
+	description?: string;
 	status: BidStatus;
-	createdAt: string; //TODO: Check if this property can use the Date type instead of string, to keep formatting, etc.
+	createdAt: string; //TODO: Check if you can type a datestring, instead of using plain strings
 }
 
-const initialState: Bid[] = [
+const initialState: BidState[] = [
 	{
 		_id: "1",
 		artisanId: "1",
 		jobId: "1",
-		bidPrice: 50000,
-		description: "A bid for a job",
+		bidPrice: "50,000",
+		// description: "A bid for a job", // Commented out so the bid uses the job's description
 		status: "Pending",
-		createdAt: new Date().toString(),
+		createdAt: new Date(2024, 9, 14, 4, 28, 52).toString(), //NOTE: The month param uses index, so January is 0, not 1.
 	},
 	{
 		_id: "2",
-		artisanId: "2",
+		artisanId: "1",
 		jobId: "2",
-		bidPrice: 50000,
-		description: "A bid for a second job",
+		bidPrice: "60,000",
+		// description: "A bid for a second job", // Commented out so the bid uses the job's description
 		status: "Pending",
-		createdAt: new Date().toString(),
+		createdAt: new Date(2024, 9, 14, 4, 28, 52).toString(), //NOTE: The month param uses index, so January is 0, not 1.
 	},
 ];
 
@@ -36,6 +38,7 @@ const bidSlice = createSlice({
 	name: "bid",
 	initialState,
 	reducers: {},
+	selectors: {},
 });
 
 export const {} = bidSlice.actions;
