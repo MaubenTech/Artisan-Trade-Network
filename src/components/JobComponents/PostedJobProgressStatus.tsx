a9ff1120bfec71955a63ceef2edb8aa0f27efe6c
Noah-V <noahvikoo3@gmail.com>
2024-10-23 20:02:23 -0500

    Noah's Commit: Actual fix to the Status data updating from jobsSlice

--- src/components/JobComponents/PostedJobProgressStatus.tsx
+++ src/components/JobComponents/PostedJobProgressStatus.tsx
@@ -9,12 +9,6 @@ import { selectCurrentUser } from "@store/usersSlice";
 import { selectJobById, selectJobsState } from "@store/jobsSlice";
 
 const PostedJobProgressStatus = ({ jobStage, bidStage }: { jobStage?: JobStatus; bidStage?: BidStatus }) => {
-    const job = useAppSelector((state) => selectJobById(state, "3"));
-
-    const jobStatus = job.status;
-
-    console.log("User's Job Stage in PostedJobProgressStatus is: " + jobStatus);
-
     const styles = compactStyles(generalStyles, androidStyles, iosStyles);
     const { type: userType } = useAppSelector(selectCurrentUser);
     const isServiceProvider = userType === "SERVICE_PROVIDER";
@@ -24,11 +18,11 @@ const PostedJobProgressStatus = ({ jobStage, bidStage }: { jobStage?: JobStatus;
     if (!isServiceProvider) {
         switch (jobStage) {
             case "Posted":
-                return <ProgressBar status={jobStatus} numerator={1} denominator={denominator} showCompleteLevel />;
+                return <ProgressBar status={jobStage} numerator={1} denominator={denominator} showCompleteLevel />;
             case "Active":
-                return <ProgressBar status={jobStatus} numerator={2} denominator={denominator} showCompleteLevel />;
+                return <ProgressBar status={jobStage} numerator={2} denominator={denominator} showCompleteLevel />;
             case "Completed":
-                return <ProgressBar status={jobStatus} numerator={3} denominator={denominator} showCompleteLevel />;
+                return <ProgressBar status={jobStage} numerator={3} denominator={denominator} showCompleteLevel />;
             default:
                 return <></>;
         }
