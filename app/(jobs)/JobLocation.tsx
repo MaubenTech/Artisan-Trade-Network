9fad10fe8f04d5d615c3ab2495ff1796c3877ab5
King Shadow <anijimjamesmaurice@gmail.com>
2024-11-02 20:54:50 +0100

    Rex's Commit: Refactored components to use information from the currentJob in the jobsSlice, instead of local parameters

--- app/(jobs)/JobLocation.tsx
+++ app/(jobs)/JobLocation.tsx
@@ -15,258 +15,246 @@ import useAppSelector from "@hooks/useAppSelector";
 import { setJobAddress } from "@store/jobsSlice";
 
 type Location = Region & {
-    longitude: number;
-    latitude: number;
-    longitudeDelta: number;
-    latitudeDelta: number;
+	longitude: number;
+	latitude: number;
+	longitudeDelta: number;
+	latitudeDelta: number;
 };
 
 const { width, height } = Dimensions.get("window");
 
 const JobLocation = () => {
-    const styles = compactStyles(generalStyles, androidStyles, iosStyles);
+	const styles = compactStyles(generalStyles, androidStyles, iosStyles);
 
-    const { jobParam } = useLocalSearchParams();
-    const newJob = JSON.parse(decodeURIComponent(jobParam as string));
+	// const { jobParam } = useLocalSearchParams();
+	// const newJob = JSON.parse(decodeURIComponent(jobParam as string));
 
-    const dispatch = useAppDispatch();
+	const newJob = useAppSelector((state) => state.jobs.currentJob);
 
-    const router = useRouter();
+	const dispatch = useAppDispatch();
 
-    const [location, setLocation] = useState<Location>({
-        longitude: 0,
-        latitude: 0,
-        longitudeDelta: 0.0922,
-        latitudeDelta: 0.0421,
-    });
+	const router = useRouter();
 
-    const job = useAppSelector((state) => state.jobs.currentJob);
+	const [location, setLocation] = useState<Location>({
+		longitude: 0,
+		latitude: 0,
+		longitudeDelta: 0.0922,
+		latitudeDelta: 0.0421,
+	});
 
-    const { address } = job;
+	const job = useAppSelector((state) => state.jobs.currentJob);
 
-    const [currentAddress, setCurrentAddress] = useState(address);
+	const { address } = job;
 
-    const [currentPosition, setCurrentPosition] = useState<string | undefined>("");
+	const [currentAddress, setCurrentAddress] = useState(address);
 
-    const getUserLocation = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
+	const [currentPosition, setCurrentPosition] = useState<string | undefined>("");
 
-        if (status !== "granted") {
-            console.log("Please grant permission");
-            return;
-        }
+	const getUserLocation = async () => {
+		let { status } = await Location.requestForegroundPermissionsAsync();
 
-        const { coords } = await Location.getCurrentPositionAsync();
+		if (status !== "granted") {
+			console.log("Please grant permission");
+			return;
+		}
 
-        const { longitude, latitude } = coords;
+		const { coords } = await Location.getCurrentPositionAsync();
 
-        setLocation({
-            longitude: coords.longitude,
-            latitude: coords.latitude,
-            longitudeDelta: 0.0922,
-            latitudeDelta: 0.0421,
-        });
+		const { longitude, latitude } = coords;
 
-        let generateAddress = await Location.reverseGeocodeAsync({
-            latitude: latitude,
-            longitude: longitude,
-        });
+		setLocation({
+			longitude: coords.longitude,
+			latitude: coords.latitude,
+			longitudeDelta: 0.0922,
+			latitudeDelta: 0.0421,
+		});
 
-        for (let item of generateAddress) {
-            let address = `${item.city} ${item.region}`;
-            // console.log(address);
-            // setCurrentAddress(address);
-            setCurrentPosition(address);
-        }
-    };
+		let generateAddress = await Location.reverseGeocodeAsync({
+			latitude: latitude,
+			longitude: longitude,
+		});
 
-    useEffect(() => {
-        getUserLocation();
-    }, []);
+		for (let item of generateAddress) {
+			let address = `${item.city} ${item.region}`;
+			// console.log(address);
+			// setCurrentAddress(address);
+			setCurrentPosition(address);
+		}
+	};
 
-    const handleProceed = () => {
-        const combinedAddress = `${currentAddress} ${currentPosition || ""}`;
+	useEffect(() => {
+		getUserLocation();
+	}, []);
 
-        const updatedJob = {
-            ...newJob,
-            address: combinedAddress,
-        };
+	const handleProceed = () => {
+		const combinedAddress = `${currentAddress} ${currentPosition || ""}`;
 
-        router.push({
-            pathname: "/JobSummary",
-            params: { jobParam: encodeURIComponent(JSON.stringify(updatedJob)) },
-        });
-    };
+		const updatedJob = {
+			...newJob,
+			address: combinedAddress,
+		};
 
-    return (
-        <CustomKeyboardView>
-            <PageHeader pageName="Location" />
-            <View style={styles.contentContainer}>
-                <View style={styles.mapContainer}>
-                    <SearchBar style={styles.searchBar} placeholder="Input your address" />
-                    <MapView
-                        style={styles.map}
-                        region={location}
-                        initialRegion={location}
-                        loadingEnabled
-                        userInterfaceStyle="dark"
-                        showsUserLocation
-                        followsUserLocation
-                    >
-                        <Marker
-                            draggable
-                            coordinate={location}
-                            onDragEnd={(event) => {
-                                let coordinate = { ...event.nativeEvent.coordinate };
-                                setLocation({
-                                    ...location,
-                                    latitude: coordinate.latitude,
-                                    longitude: coordinate.longitude,
-                                });
-                            }}
-                        />
-                    </MapView>
-                </View>
-                <ScrollView style={styles.addressContainer}>
-                    <View style={styles.modalTitleContainer}>
-                        <Text style={styles.modalTitle}>Address</Text>
-                        <View style={styles.fullAddress}>
-                            <Text>{currentPosition}</Text>
-                        </View>
-                    </View>
-                    <View style={styles.addedInformationContainer}>
-                        <Text style={styles.addedInfoTitle}>Added Information</Text>
-                        <TextInput
-                            style={styles.addedInformation}
-                            multiline
-                            numberOfLines={10}
-                            value={address}
-                            onChangeText={(text: string) => dispatch(setJobAddress(text))}
-                        />
-                    </View>
-                    <ButtonGroup negativeOption="Cancel" positiveOption="Proceed" onPress={handleProceed} reverse />
-                </ScrollView>
-            </View>
-        </CustomKeyboardView>
-    );
+		router.push({
+			pathname: "/JobSummary",
+			params: { jobParam: encodeURIComponent(JSON.stringify(updatedJob)) },
+		});
+	};
+
+	return (
+		<CustomKeyboardView>
+			<PageHeader pageName="Location" />
+			<View style={styles.contentContainer}>
+				<View style={styles.mapContainer}>
+					<SearchBar style={styles.searchBar} placeholder="Input your address" />
+					<MapView style={styles.map} region={location} initialRegion={location} loadingEnabled userInterfaceStyle="dark" showsUserLocation followsUserLocation>
+						<Marker
+							draggable
+							coordinate={location}
+							onDragEnd={(event) => {
+								let coordinate = { ...event.nativeEvent.coordinate };
+								setLocation({
+									...location,
+									latitude: coordinate.latitude,
+									longitude: coordinate.longitude,
+								});
+							}}
+						/>
+					</MapView>
+				</View>
+				<ScrollView style={styles.addressContainer}>
+					<View style={styles.modalTitleContainer}>
+						<Text style={styles.modalTitle}>Address</Text>
+						<View style={styles.fullAddress}>
+							<Text>{currentPosition}</Text>
+						</View>
+					</View>
+					<View style={styles.addedInformationContainer}>
+						<Text style={styles.addedInfoTitle}>Added Information</Text>
+						<TextInput style={styles.addedInformation} multiline numberOfLines={10} value={address} onChangeText={(text: string) => dispatch(setJobAddress(text))} />
+					</View>
+					<ButtonGroup negativeOption="Cancel" positiveOption="Proceed" onPress={handleProceed} reverse />
+				</ScrollView>
+			</View>
+		</CustomKeyboardView>
+	);
 };
 
 export default JobLocation;
 
 const generalStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        // gap: 10,
-    },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		// gap: 10,
+	},
 
-    contentContainer: {
-        height: "100%",
-        width: "100%",
-        position: "relative",
-    },
+	contentContainer: {
+		height: "100%",
+		width: "100%",
+		position: "relative",
+	},
 
-    mapContainer: {
-        width: "100%",
-        height: "50%",
-        position: "relative",
-    },
+	mapContainer: {
+		width: "100%",
+		height: "50%",
+		position: "relative",
+	},
 
-    inputAddressContainer: {
-        position: "absolute",
-        width: "90%",
-    },
+	inputAddressContainer: {
+		position: "absolute",
+		width: "90%",
+	},
 
-    map: {
-        height: "100%",
-        // width: "100%",
-        // position: "absolute",
-        zIndex: 0,
-    },
+	map: {
+		height: "100%",
+		// width: "100%",
+		// position: "absolute",
+		zIndex: 0,
+	},
 
-    modalTitleContainer: {
-        gap: 10,
-    },
+	modalTitleContainer: {
+		gap: 10,
+	},
 
-    modalTitle: {
-        fontWeight: "600",
-        fontSize: 20,
-    },
+	modalTitle: {
+		fontWeight: "600",
+		fontSize: 20,
+	},
 
-    fullAddress: {},
+	fullAddress: {},
 
-    addedInformationContainer: {
-        gap: 10,
-    },
+	addedInformationContainer: {
+		gap: 10,
+	},
 
-    addedInfoTitle: {
-        fontSize: 16,
-        fontWeight: "600",
-    },
+	addedInfoTitle: {
+		fontSize: 16,
+		fontWeight: "600",
+	},
 
-    addedInformation: {
-        textAlignVertical: "top",
-        height: 110,
-        padding: 10,
-        borderWidth: 1,
-        borderRadius: 10,
-        width: "100%",
-        borderColor: colors.inputBorderColor,
-    },
+	addedInformation: {
+		textAlignVertical: "top",
+		height: 110,
+		padding: 10,
+		borderWidth: 1,
+		borderRadius: 10,
+		width: "100%",
+		borderColor: colors.inputBorderColor,
+	},
 });
 
 const androidStyles = StyleSheet.create({
-    searchBar: {
-        position: "absolute",
-        top: "14%",
-        zIndex: 1,
-        backgroundColor: "#fff",
-        alignSelf: "center",
-        width: width * 0.9,
-        padding: 10,
-    },
+	searchBar: {
+		position: "absolute",
+		top: "14%",
+		zIndex: 1,
+		backgroundColor: "#fff",
+		alignSelf: "center",
+		width: width * 0.9,
+		padding: 10,
+	},
 
-    addressContainer: {
-        paddingHorizontal: 30,
-        borderWidth: 1,
-        borderColor: colors.greySecondaryShade,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        position: "absolute",
-        top: "45%",
-        backgroundColor: colors.white,
-        // width: "100%",
-        // height: "100%",
-        zIndex: 1,
-        paddingVertical: 20,
-        gap: 10,
-    },
+	addressContainer: {
+		paddingHorizontal: 30,
+		borderWidth: 1,
+		borderColor: colors.greySecondaryShade,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+		position: "absolute",
+		top: "45%",
+		backgroundColor: colors.white,
+		// width: "100%",
+		// height: "100%",
+		zIndex: 1,
+		paddingVertical: 20,
+		gap: 10,
+	},
 });
 
 const iosStyles = StyleSheet.create({
-    searchBar: {
-        position: "absolute",
-        top: "5%",
-        zIndex: 1,
-        backgroundColor: "#fff",
-        alignSelf: "center",
-        width: width * 0.9,
-    },
+	searchBar: {
+		position: "absolute",
+		top: "5%",
+		zIndex: 1,
+		backgroundColor: "#fff",
+		alignSelf: "center",
+		width: width * 0.9,
+	},
 
-    addressContainer: {
-        paddingHorizontal: 30,
-        borderWidth: 1,
-        borderColor: colors.greySecondaryShade,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        position: "absolute",
-        top: "45%",
-        backgroundColor: colors.white,
-        // width: "100%",
-        // height: "100%",
-        zIndex: 1,
-        paddingVertical: 20,
-        gap: 10,
-    },
+	addressContainer: {
+		paddingHorizontal: 30,
+		borderWidth: 1,
+		borderColor: colors.greySecondaryShade,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+		position: "absolute",
+		top: "45%",
+		backgroundColor: colors.white,
+		// width: "100%",
+		// height: "100%",
+		zIndex: 1,
+		paddingVertical: 20,
+		gap: 10,
+	},
 });
--- app/(jobs)/JobSummary.tsx
+++ app/(jobs)/JobSummary.tsx
@@ -9,163 +9,154 @@ import ButtonGroup from "../../src/components/ButtonGroup";
 import { ImagePickerAsset } from "expo-image-picker";
 import { addNewJob, Job, resetCurrentJob } from "@store/jobsSlice";
 import useAppDispatch from "@hooks/useAppDispatch";
+import useAppSelector from "@hooks/useAppSelector";
 
 const JobSummary = () => {
-    const { jobParam } = useLocalSearchParams();
+	const newJob = useAppSelector((state) => state.jobs.currentJob);
+	const { media } = newJob;
 
-    const newJob: Job = JSON.parse(decodeURIComponent(jobParam as string));
-    const { media } = newJob;
+	const handleJobSubmission = () => {
+		router.push({
+			pathname: "/JobPosted",
+			params: { jobParam: encodeURIComponent(JSON.stringify(newJob)) },
+		});
+	};
 
-    const DecodedImagesComponent = () => {
-        return (
-            <>
-                {media.map((imageUri: ImagePickerAsset, index: number) => {
-                    return <Image source={{ uri: imageUri.uri }} style={styles.uploadedImage} key={index} />;
-                })}
-            </>
-        );
-    };
-
-    const handleJobSubmission = () => {
-        router.push({
-            pathname: "/JobPosted",
-            params: { jobParam: encodeURIComponent(JSON.stringify(newJob)) },
-        });
-    };
-
-    return (
-        <View style={styles.container}>
-            <PageHeader pageName="Summary" />
-            <ScrollView style={styles.contentContainer}>
-                <View style={styles.summaryTitleContainer}>
-                    <View style={styles.summaryTitleSubContainer}>
-                        <Text style={styles.summaryTitle}>Job Title</Text>
-                        <Text style={styles.summarySubTitle}>{newJob.title}</Text>
-                    </View>
-                    <View style={styles.summaryEditContainer}>
-                        <Text style={styles.editText}>Edit</Text>
-                        <Ionicons name="pencil" />
-                    </View>
-                </View>
-                <View style={styles.summaryTitleContainer}>
-                    <View style={styles.summaryTitleSubContainer}>
-                        <Text style={styles.summaryTitle}>Job Type</Text>
-                        <Text style={styles.summarySubTitle}>{newJob.type}</Text>
-                    </View>
-                    <View style={styles.summaryEditContainer}>
-                        <Text style={styles.editText}>Edit</Text>
-                        <Ionicons name="pencil" />
-                    </View>
-                </View>
-                <View style={styles.summaryTitleContainer}>
-                    <View style={styles.summaryTitleSubContainer}>
-                        <Text style={styles.summaryTitle}>Job Description</Text>
-                        <Text style={styles.summarySubTitle}>{newJob.description}</Text>
-                    </View>
-                    <View style={styles.summaryEditContainer}>
-                        <Text style={styles.editText}>Edit</Text>
-                        <Ionicons name="pencil" />
-                    </View>
-                </View>
-                <View style={styles.summaryTitleContainer}>
-                    <View style={styles.summaryTitleSubContainer}>
-                        <Text style={styles.summaryTitle}>Budget</Text>
-                        <Text style={styles.summarySubTitle}>{newJob.budget}</Text>
-                    </View>
-                    <View style={styles.summaryEditContainer}>
-                        <Text style={styles.editText}>Edit</Text>
-                        <Ionicons name="pencil" />
-                    </View>
-                </View>
-                <View style={[styles.summaryTitleContainer, { flexDirection: "column" }]}>
-                    <Text style={styles.summaryTitle}>Media</Text>
-                    <View style={{ flexDirection: "row", gap: 10 }}>
-                        <DecodedImagesComponent />
-                    </View>
-                </View>
-                <View style={styles.summaryTitleContainer}>
-                    <View style={styles.summaryTitleSubContainer}>
-                        <Text style={styles.summaryTitle}>Address</Text>
-                        <Text style={styles.summarySubTitle}>{newJob.address}</Text>
-                    </View>
-                    <View style={styles.summaryEditContainer}>
-                        <Text style={styles.editText}>Edit</Text>
-                        <Ionicons name="pencil" />
-                    </View>
-                </View>
-                <View style={{ marginBottom: 60 }}>
-                    <ButtonGroup
-                        paddingHorizontal={20}
-                        positiveOption="Post Job"
-                        positiveOptionBg={colors.greenShade}
-                        // href={"/(customerPages)/JobPosted"}
-                        onPress={handleJobSubmission}
-                    />
-                </View>
-            </ScrollView>
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<PageHeader pageName="Summary" />
+			<ScrollView style={styles.contentContainer}>
+				<View style={styles.summaryTitleContainer}>
+					<View style={styles.summaryTitleSubContainer}>
+						<Text style={styles.summaryTitle}>Job Title</Text>
+						<Text style={styles.summarySubTitle}>{newJob.title}</Text>
+					</View>
+					<View style={styles.summaryEditContainer}>
+						<Text style={styles.editText}>Edit</Text>
+						<Ionicons name="pencil" />
+					</View>
+				</View>
+				<View style={styles.summaryTitleContainer}>
+					<View style={styles.summaryTitleSubContainer}>
+						<Text style={styles.summaryTitle}>Job Type</Text>
+						<Text style={styles.summarySubTitle}>{newJob.type}</Text>
+					</View>
+					<View style={styles.summaryEditContainer}>
+						<Text style={styles.editText}>Edit</Text>
+						<Ionicons name="pencil" />
+					</View>
+				</View>
+				<View style={styles.summaryTitleContainer}>
+					<View style={styles.summaryTitleSubContainer}>
+						<Text style={styles.summaryTitle}>Job Description</Text>
+						<Text style={styles.summarySubTitle}>{newJob.description}</Text>
+					</View>
+					<View style={styles.summaryEditContainer}>
+						<Text style={styles.editText}>Edit</Text>
+						<Ionicons name="pencil" />
+					</View>
+				</View>
+				<View style={styles.summaryTitleContainer}>
+					<View style={styles.summaryTitleSubContainer}>
+						<Text style={styles.summaryTitle}>Budget</Text>
+						<Text style={styles.summarySubTitle}>{newJob.budget}</Text>
+					</View>
+					<View style={styles.summaryEditContainer}>
+						<Text style={styles.editText}>Edit</Text>
+						<Ionicons name="pencil" />
+					</View>
+				</View>
+				<View style={[styles.summaryTitleContainer, { flexDirection: "column" }]}>
+					<Text style={styles.summaryTitle}>Media</Text>
+					<View style={{ flexDirection: "row", gap: 10 }}>
+						{media.map((imageUri: ImagePickerAsset, index: number) => {
+							return <Image source={{ uri: imageUri.uri }} style={styles.uploadedImage} key={index} />;
+						})}
+					</View>
+				</View>
+				<View style={styles.summaryTitleContainer}>
+					<View style={styles.summaryTitleSubContainer}>
+						<Text style={styles.summaryTitle}>Address</Text>
+						<Text style={styles.summarySubTitle}>{newJob.address}</Text>
+					</View>
+					<View style={styles.summaryEditContainer}>
+						<Text style={styles.editText}>Edit</Text>
+						<Ionicons name="pencil" />
+					</View>
+				</View>
+				<View style={{ marginBottom: 60 }}>
+					<ButtonGroup
+						paddingHorizontal={20}
+						positiveOption="Post Job"
+						positiveOptionBg={colors.greenShade}
+						// href={"/(customerPages)/JobPosted"}
+						onPress={handleJobSubmission}
+					/>
+				</View>
+			</ScrollView>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        gap: 10,
-    },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		gap: 10,
+	},
 
-    contentContainer: {
-        paddingTop: 20,
-    },
+	contentContainer: {
+		paddingTop: 20,
+	},
 
-    summaryTitleContainer: {
-        flexDirection: "row",
-        borderBottomWidth: 1,
-        borderBottomColor: colors.greyBorder,
-        paddingBottom: 10,
-        paddingHorizontal: 30,
-        justifyContent: "space-between",
-        alignItems: "flex-start",
-        padding: 20,
-    },
+	summaryTitleContainer: {
+		flexDirection: "row",
+		borderBottomWidth: 1,
+		borderBottomColor: colors.greyBorder,
+		paddingBottom: 10,
+		paddingHorizontal: 30,
+		justifyContent: "space-between",
+		alignItems: "flex-start",
+		padding: 20,
+	},
 
-    summaryTitleSubContainer: {
-        width: "80%",
-    },
+	summaryTitleSubContainer: {
+		width: "80%",
+	},
 
-    summaryTitle: {
-        fontSize: 20,
-        fontWeight: "600",
-    },
+	summaryTitle: {
+		fontSize: 20,
+		fontWeight: "600",
+	},
 
-    summarySubTitle: {
-        color: colors.greySecondaryShade,
-    },
+	summarySubTitle: {
+		color: colors.greySecondaryShade,
+	},
 
-    editText: {
-        textDecorationLine: "underline",
-    },
+	editText: {
+		textDecorationLine: "underline",
+	},
 
-    summaryEditContainer: {
-        flexDirection: "row",
-        gap: 5,
-    },
+	summaryEditContainer: {
+		flexDirection: "row",
+		gap: 5,
+	},
 
-    uploadedMediaContainer: {
-        paddingHorizontal: 20,
-        width: "100%",
-        gap: 15,
-        marginTop: 20,
-        flexDirection: "row",
-    },
+	uploadedMediaContainer: {
+		paddingHorizontal: 20,
+		width: "100%",
+		gap: 15,
+		marginTop: 20,
+		flexDirection: "row",
+	},
 
-    uploadedImage: {
-        height: 100,
-        width: 100,
-        padding: "7%",
-        alignItems: "center",
-        borderRadius: 10,
-    },
+	uploadedImage: {
+		height: 100,
+		width: 100,
+		padding: "7%",
+		alignItems: "center",
+		borderRadius: 10,
+	},
 });
 
 export default JobSummary;
