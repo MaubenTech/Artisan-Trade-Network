83d119c2c9c360246615e100f4e7a23b98bf0575
King Shadow <anijimjamesmaurice@gmail.com>
2024-11-07 23:00:35 +0100

    Rex's Commit: Further tweaks to the bidsSlice. Memoized the selectBidsByJobId as it returns a new array reference using Array.filter everytime it's called

--- src/store/bidsSlice.ts
+++ src/store/bidsSlice.ts
@@ -1,10 +1,10 @@
 import createAppSelector from "@hooks/createAppSelector";
 import { createSlice } from "@reduxjs/toolkit";
 import { BidJob, BidStatus } from "app/(tabs)/Bids";
-import { selectJobById, selectJobsState } from "./jobsSlice";
+import { selectAllJobs, selectJobById, selectJobsState } from "./jobsSlice";
 import { RootState } from "@store";
 
-export interface BidState {
+export interface Bid {
 	_id: string;
 	artisanId: string;
 	jobId: string;
@@ -14,74 +14,74 @@ export interface BidState {
 	createdAt: string; //TODO: Check if you can type a datestring, instead of using plain strings
 }
 
-const initialState: BidState[] = [
+const initialState: Bid[] = [
 	{
 		_id: "1",
 		artisanId: "1",
-		jobId: "1",
+		jobId: "5",
 		bidPrice: "50,000",
 		description: "I'm only available on weekends", // Commented out so the bid uses the job's description
-		status: "Pending",
+		status: "Bid",
 		createdAt: new Date(2024, 9, 14, 4, 28, 52).toString(), //NOTE: The month param uses index, so January is 0, not 1.
 	},
 	{
 		_id: "2",
-		artisanId: "1",
-		jobId: "2",
+		artisanId: "4",
+		jobId: "5",
 		bidPrice: "60,000",
 		description: "When will you be available for the job?", // Commented out so the bid uses the job's description
-		status: "Pending",
+		status: "Bid",
 		createdAt: new Date(2024, 9, 14, 4, 28, 52).toString(), //NOTE: The month param uses index, so January is 0, not 1.
 	},
 	{
 		_id: "3",
 		artisanId: "1",
-		jobId: "3",
+		jobId: "6",
+		bidPrice: "100,000",
+		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
+		status: "Bid",
+		createdAt: new Date(2024, 9, 14, 17, 50, 18).toString(), //NOTE: The month param uses index, so January is 0, not 1.
+	},
+	{
+		_id: "4",
+		artisanId: "4",
+		jobId: "6",
+		bidPrice: "100,000",
+		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
+		status: "Bid",
+		createdAt: new Date(2024, 9, 14, 17, 50, 18).toString(), //NOTE: The month param uses index, so January is 0, not 1.
+	},
+	{
+		_id: "5",
+		artisanId: "1",
+		jobId: "8",
+		bidPrice: "100,000",
+		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
+		status: "Bid",
+		createdAt: new Date(2024, 9, 14, 17, 50, 18).toString(), //NOTE: The month param uses index, so January is 0, not 1.
+	},
+	{
+		_id: "6",
+		artisanId: "4",
+		jobId: "8",
+		bidPrice: "100,000",
+		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
+		status: "Bid",
+		createdAt: new Date(2024, 9, 14, 17, 50, 18).toString(), //NOTE: The month param uses index, so January is 0, not 1.
+	},
+	{
+		_id: "7",
+		artisanId: "1",
+		jobId: "9",
 		bidPrice: "100,000",
 		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
 		status: "Pending",
 		createdAt: new Date(2024, 9, 14, 17, 50, 18).toString(), //NOTE: The month param uses index, so January is 0, not 1.
 	},
 	{
-		_id: "3",
-		artisanId: "1",
-		jobId: "3",
-		bidPrice: "100,000",
-		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
-		status: "Pending",
-		createdAt: new Date(2024, 9, 14, 17, 50, 18).toString(), //NOTE: The month param uses index, so January is 0, not 1.
-	},
-	{
-		_id: "3",
-		artisanId: "1",
-		jobId: "3",
-		bidPrice: "100,000",
-		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
-		status: "Pending",
-		createdAt: new Date(2024, 9, 14, 17, 50, 18).toString(), //NOTE: The month param uses index, so January is 0, not 1.
-	},
-	{
-		_id: "3",
-		artisanId: "1",
-		jobId: "3",
-		bidPrice: "100,000",
-		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
-		status: "Pending",
-		createdAt: new Date(2024, 9, 14, 17, 50, 18).toString(), //NOTE: The month param uses index, so January is 0, not 1.
-	},
-	{
-		_id: "3",
-		artisanId: "1",
-		jobId: "3",
-		bidPrice: "100,000",
-		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
-		status: "Pending",
-		createdAt: new Date(2024, 9, 14, 17, 50, 18).toString(), //NOTE: The month param uses index, so January is 0, not 1.
-	},
-	{
-		_id: "3",
-		artisanId: "1",
-		jobId: "3",
+		_id: "8",
+		artisanId: "4",
+		jobId: "10",
 		bidPrice: "100,000",
 		description: "I'll help you fix it in no time!", // Commented out so the bid uses the job's description
 		status: "Pending",
@@ -94,19 +94,23 @@ const bidSlice = createSlice({
 	initialState,
 	reducers: {},
 	selectors: {
-		selectBids: (state: BidState[]) => state,
-		selectBidById: (state: BidState[], bidId: string) => state.find((bid) => bid._id === bidId),
+		selectBids: (state: Bid[]) => state,
+		selectBidById: (state: Bid[], bidId: string) => state.find((bid) => bid._id === bidId),
+		selectFirstBidByJobId: (state: Bid[], jobId: string) => state.find((bid) => bid.jobId === jobId),
 		// selectBidJob: (state: BidState[], bidId: string) => state
 	},
 });
 
 export const {} = bidSlice.actions;
 
-export const { selectBids, selectBidById } = bidSlice.selectors;
+export const { selectBids, selectBidById, selectFirstBidByJobId } = bidSlice.selectors;
 
-export const selectBidJobByBidId = createAppSelector([(state: RootState) => state, selectBidById], (state, bid) => {
-	//FIXME: Input selectors should never use state => state as said by redux! https://redux.js.org/usage/deriving-data-selectors#optimizing-selectors-with-memoization:~:text=Similarly%2C%20a%20memoized%20selector%20should%20never%20use%20state%20%3D%3E%20state%20as%20an%20input!%20That%20will%20force%20the%20selector%20to%20always%20recalculate.
-	const job = selectJobById(state, bid.jobId);
+export const selectBidsByJobId = createAppSelector([selectBids, (state, jobId: string) => jobId], (bids, jobId) => {
+	return bids.filter((bid) => bid.jobId === jobId);
+});
+
+export const selectBidJobByBidId = createAppSelector([selectAllJobs, selectBidById], (jobList, bid) => {
+	const job = selectJobById(jobList, bid.jobId);
 	const bidJob: BidJob = { ...job, ...bid };
 	return bidJob;
 });
